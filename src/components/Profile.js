6956914bb36f1b9ad32c9ba4e1a61a020e0b3064
joaosf <joaosf7@gmail.com>
2024-02-27 18:17:53 +0000

    removed alt img

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,7 @@ function Profile(){
     return (
         <div className='container'>
             <h1 className='title'>{DATA.name}</h1>
-            <img className='char-image' src={require('../assets/kim.png')}  />
+            <img className='char-image' src={require('../assets/kim.png')} alt='' />
             <p className='description'>{DATA.description}</p>
             <ul className="profile-list">
                 {console.log(DATA.socialMedia)}
